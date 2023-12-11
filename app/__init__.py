e325440a0b0712cbc13e804bd2198ee4d0e053f6
RossA2425 <98879596+RossA2425@users.noreply.github.com>
2023-12-11 14:32:30 +0000

    feat: Setup API methods initially

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,11 @@ from .routes.recipe import recipe_blueprint
 
 def create_app():
     app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return "Welcome to Cupboard Recipes API!"
+
     app.register_blueprint(recipe_blueprint, url_prefix='/recipe')
+
     return app
--- app/routes/recipe.py
+++ app/routes/recipe.py
@@ -2,6 +2,10 @@ from flask import Blueprint
 
 recipe_blueprint = Blueprint('recipe', __name__)
 
+@recipe_blueprint.route('/')
+def recipe_home():
+    return "select a valid method to query"
+
 @recipe_blueprint.route('/search_recipe', methods=['GET'])
 def search_recipe():
     # Recipe search logic
